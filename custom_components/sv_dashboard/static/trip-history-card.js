af0f6d93005f3e7af5ed18d55a139ad8be15f088
CaneTLOTW <35076818+CaneTLOTW@users.noreply.github.com>
2026-09-21 15:28:43 +0200

    Release: bump shared i18n cache key to beta28

--- custom_components/sv_dashboard/static/trip-history-card.js
+++ custom_components/sv_dashboard/static/trip-history-card.js
@@ -1,5 +1,5 @@
 import { LitElement, html, css, nothing } from "./vendor-lit.js?v=0.6.0-beta.7";
-import { localeFor, textFor } from "./i18n.js?v=0.6.0-beta.27";
+import { localeFor, textFor } from "./i18n.js?v=0.6.0-beta.28";
 import { insertOdometerGapRows, semanticTripTime, tripFilterTime } from "./trip-gap-core.js?v=0.6.0-beta.15";
 
 /**
